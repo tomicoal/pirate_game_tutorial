127ab9488d3f5b752e9251fa37be2a403a7bfa65
Tomas <tmico@gmail.com>
2023-11-30 12:27:23 +0000

    create game_data

--- code/main.py
+++ code/main.py
@@ -2,12 +2,13 @@ import pygame
 import sys
 from settings import *
 from level import Level
+from game_data import level_0
 
 pygame.init()
 screen = pygame.display.set_mode((screen_with, screen_height))  # Height / width
 pygame.display.set_caption(title="Pirate Hunt")
 clock = pygame.time.Clock()
-level = Level(level_map, screen)
+level = Level(level_0, screen)
 
 while True:
     for event in pygame.event.get():
